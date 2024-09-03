f59db5290cc00961cbe32468f18703f6137dc828
EmpiresHQ <info@4j.ee>
2024-09-03 15:32:30 +0300

    fix: familyId for lemana request

--- dist/sites/lemanapro.ru/settings.js
+++ dist/sites/lemanapro.ru/settings.js
@@ -26,7 +26,7 @@ export const treeRootSettings = {
     perPage: -1
 };
 export const apiRequestOpts = (handler, page = 0) => {
-    var _a, _b, _c, _d, _e, _f, _g;
+    var _a, _b, _c, _d, _e, _f;
     const token = ((_b = (_a = handler.data.meta) === null || _a === void 0 ? void 0 : _a.antibotData) === null || _b === void 0 ? void 0 : _b.evaluatedValue) ?
         ((_d = (_c = handler.data.meta) === null || _c === void 0 ? void 0 : _c.antibotData) === null || _d === void 0 ? void 0 : _d.evaluatedValue).env.apiKey : "Yeg8l3zQDwpVNBDTP3q6jM4lQVLW5TTv";
     return {
@@ -40,11 +40,11 @@ export const apiRequestOpts = (handler, page = 0) => {
         remoteCategoryId: (_e = handler.data.remoteId) !== null && _e !== void 0 ? _e : "",
         page,
         payload: {
-            familyIds: [(_g = (_f = handler.data.meta) === null || _f === void 0 ? void 0 : _f.key) !== null && _g !== void 0 ? _g : ""],
+            familyIds: [(_f = handler.data.remoteId) !== null && _f !== void 0 ? _f : ""],
             regionId: "34",
             suggest: true,
             limit: 30,
-            offset: page > 0 ? page * 30 : 0
+            offset: page > 1 ? page - 1 * 30 : 0
         },
     };
 };
