229be6002c8c95db66676ae89255029bbc154282
EmpiresHQ <info@4j.ee>
2024-11-01 12:00:57 +0200

    chore: fetcher type

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,17 +1,19 @@
 import { BaseProcessorError } from "./error.js";
 import { BaseItem } from "./item.js";
-import { InitialSettings, SettingsHandler } from "./settings.js";
+import { InitialSettings, RequestParameters, SettingsHandler } from "./settings.js";
 
 export * from "./item.js";
 export * from "./settings.js";
 export * from "./base.js";
 export * from "./category.js";
 
-// eslint-disable-next-line @typescript-eslint/ban-types, @typescript-eslint/no-explicit-any
-export type CategoryParser<T extends Object = any> = (args: {
+export type ParserResponseType<T extends Object = any> = {
   html?: Buffer;
   json?: T;
-}) => Promise<{
+};
+
+// eslint-disable-next-line @typescript-eslint/ban-types, @typescript-eslint/no-explicit-any
+export type CategoryParser<T extends Object = any> = (args: ParserResponseType<T>) => Promise<{
   items?: BaseItem[];
   hasNextPage?: boolean;
   err?: BaseProcessorError;
@@ -22,4 +24,5 @@ export type StrategyHandler<T extends Object = any> = {
   parser: CategoryParser;
   opts: SettingsHandler<T>;
   settings: InitialSettings;
+  fetcher?: <T extends Object = any>(params: RequestParameters) => ParserResponseType<T>
 };
